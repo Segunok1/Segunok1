c2ad276f2c9b166c213f20450167a04947e00a54
Segunok1 <k077ma777@yandex.ru>
2022-08-17 00:58:03 +0300

    Example012 Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,77 +1,150 @@
-﻿// Вид 1  Метод ни чего не возвращает.
+﻿// // Вид 1  Метод ни чего не возвращает.
 
-void Method1();
+// void Method1();
+// {
+//     Console.WriteLine();
+// }
+// Method1();
+
+// // Вид 2  Может принимать
+// // какие либо аргументы. Метод ни чего не возвращает.
+
+// void Method2(string msg);
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Текст сообщения", count: 4);
+// Method21(count: 4, msg: "Новый текст");
+
+
+// // Вид 3 Метод может что-то возвращать, но не принимать ни
+// // какич аргументов.
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// // Вид 4   Может принимать какие либо данные.
+// // И возвращает какие либо аргументы.
+
+// int Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while(i < count)
+//     {
+//         result += text;
+//         i++;
+//     }
+//     return result;
+// }
+
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+// // С циклом for
+
+// int Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for(int i = 0; i < count; i++) 
+//     {
+//         result += text;
+//     }
+//     return result;
+// }
+
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// // Таблица умножения
+
+// for(int i = 0; i <= 10; i++)
+// {
+//     for(int j = 2; i <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// // Работа с текстом
+
+// // Дан текст. В тексте нужно все пробеллы заменить 
+// // На чёрточки
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for(int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }  
+//     return result;
+//  }
+
+
+//  string newText = Replace(text, ' ', '|');
+//  Console.WriteLine(newText);
+
+// Алгоритм сортировки методом выбора .
+
+int[] array = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
 {
+    int count = array.Length;   
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
     Console.WriteLine();
 }
-Method1();
 
-// Вид 2  Может принимать
-// какие либо аргументы. Метод ни чего не возвращает.
-
-void Method2(string msg);
+void SelectionSort(int[] array)
 {
-    Console.WriteLine(msg);
-}
-Method2(msg: "Текст сообщения");
-
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
+    for(int i = 0; i < array.Length - 1; i++)
     {
-        Console.WriteLine(msg);
-        i++;
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) 
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
     }
 }
-Method21(msg: "Текст сообщения", count: 4);
-Method21(count: 4, msg: "Новый текст");
 
 
-// Вид 3 Метод может что-то возвращать, но не принимать ни
-// какич аргументов.
+PrintArray(array);
+SelectionSort(array);
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-
-int year = Method3();
-Console.WriteLine(year);
-
-
-// Вид 4   Может принимать какие либо данные.
-// И возвращает какие либо аргументы.
-
-int Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
-
-    while(i < count)
-    {
-        result += text;
-        i++;
-    }
-    return result;
-}
-
-
-string res = Method4(10, "asdf");
-Console.WriteLine(res);
-
-// С циклом for
-
-int Method4(int count, string text)
-{
-    string result = String.Empty;
-    for(int i = 0; i < count, i++;) 
-    {
-        result += text;
-    }
-    return result;
-}
-
-
-string res = Method4(10, "z");
-Console.WriteLine(res);
+PrintArray(array);
